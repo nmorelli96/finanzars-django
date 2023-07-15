a965155dc31fcd46e565b89e34217538cf87181d
nmorelli96 <morellinicolas96@gmail.com>
2023-07-15 15:24:50 -0300

    Change settings.py

--- finanzars_root/finanzars/settings.py
+++ finanzars_root/finanzars/settings.py
@@ -19,11 +19,11 @@ import dj_database_url
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-DATABASES = {
+'''DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
-}
+}'''
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
