cc0dd592c85463db6ac7fdd9ff588173b362d0de
nmorelli96 <morellinicolas96@gmail.com>
2023-08-07 01:10:53 -0300

    Cambiar idioma a español

--- finanzars_root/finanzars/settings.py
+++ finanzars_root/finanzars/settings.py
@@ -131,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
 TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
