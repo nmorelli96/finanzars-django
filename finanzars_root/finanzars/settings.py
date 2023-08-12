e466690f46c32d944fd1ce1159c41bd46ee7e618
nmorelli96 <morellinicolas96@gmail.com>
2023-08-11 23:36:33 -0300

    Update settings

--- finanzars_root/finanzars/settings.py
+++ finanzars_root/finanzars/settings.py
@@ -149,6 +149,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
