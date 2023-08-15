79a93a604647184ddec347ab3b301e5ea9afde65
nmorelli96 <morellinicolas96@gmail.com>
2023-08-15 20:22:59 -0300

    Agregar email de bienvenida

--- finanzars_root/cuentas/views.py
+++ finanzars_root/cuentas/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
 
 from .forms import RegistroForm
 
@@ -13,6 +16,15 @@ def registro(request):
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
+
+            subject = 'Bienvenido a FinanzARS'
+            context = {'username': user.username}
+            html_message = render_to_string('cuentas/welcome_email.html', context)
+            plain_message = strip_tags(html_message)  # Convertir HTML a texto plano
+            from_email = settings.DEFAULT_FROM_EMAIL
+            recipient_list = [user.email]
+            send_mail(subject, plain_message, from_email, recipient_list, html_message=html_message)
+
             return redirect("tipos")
     else:
         form = RegistroForm()
--- finanzars_root/instrumento/management/commands/send_test_email.py
+++ finanzars_root/instrumento/management/commands/send_test_email.py
@@ -1,6 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
 
+'''
 class Command(BaseCommand):
     help = 'Envía un correo electrónico de prueba.'
 
@@ -11,5 +15,20 @@ class Command(BaseCommand):
         recipient_list = ['morellinicolas96@gmail.com']
 
         send_mail(subject, message, from_email, recipient_list)
+
+        self.stdout.write(self.style.SUCCESS('Correo electrónico de prueba enviado correctamente.'))
+'''
+
+class Command(BaseCommand):
+    help = 'Envía un correo electrónico de prueba.'
+
+    def handle(self, *args, **options):
+        subject = 'Prueba de correo electrónico'
+        html_message = render_to_string('cuentas/welcome_email.html')
+        plain_message = strip_tags(html_message)  # Convertir HTML a texto plano
+        from_email = 'cuentas@finanzars.com.ar'
+        recipient_list = ['morellinicolas96@gmail.com']
+
+        send_mail(subject, plain_message, from_email, recipient_list, html_message=html_message)
 
         self.stdout.write(self.style.SUCCESS('Correo electrónico de prueba enviado correctamente.'))
