acbfcd6973109ca67f9d75b66632242824981a84
nmorelli96 <morellinicolas96@gmail.com>
2023-08-27 23:33:17 -0300

    Cambiar scrap_usa de selenium a get request

--- finanzars_root/instrumento/management/commands/scrap_usa.py
+++ finanzars_root/instrumento/management/commands/scrap_usa.py
@@ -1,80 +1,38 @@
+import json
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
-import time
-import os
-import glob
+import requests
 from datetime import datetime
-from instrumento.management.activos_lists import usa_list
 
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 #chrome_options.add_argument('user-agent={0}'.format(user_agent)) # para evitar access denied en nasdaq
 
 def scrap_usa():
-    options = Options()
-    options.headless = True
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument('user-agent={0}'.format(user_agent)) # para evitar access denied en nasdaq
-    #options.add_argument("--disable-gpu")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-    download_path = r"C:\Users\Nicolas\Documents\REPOS\finanzars-cartera\finanzars_root\instrumento\resources"
-
-    prefs = {
-        "download.default_directory": download_path,
+    header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest"
     }
-    options.add_experimental_option("prefs", prefs)
 
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    driver.get('https://www.nasdaq.com/market-activity/stocks/screener')
-    driver.implicitly_wait(15)
-    #driver.get_screenshot_as_file("screenshot.png") #check web before finding element
-    container = driver.find_element(By.CLASS_NAME, "nasdaq-screener__content-container")
-    csv_button = container.find_elements(By.TAG_NAME, "button")
-    ActionChains(driver).click(csv_button[0]).perform()
-    time.sleep(8)
-    driver.close() 
+    usa_list = ["AMZN", "AAPL", "GOOGL", "KO", "NIO", "COIN", "PBR", "BB", "AMD", "BABA", "DIS", "MELI", "VIST", "TSLA", "SHOP", "MSFT", "JNJ", "BBD", "INTC", "AGRO", "NKE", "NVDA", "HMY", "UPST", "VALE", "AXP", "TS", "OXY", "BRK/B", "T", "ABNB", "ARCO", "GOLD", "META", "BRFS", "PYPL", "BITF", "SE", "X", "GLOB", "PFE", "PAAS", "BAC", "SATL", "ZM", "JMIA", "CAAP", "WMT", "C", "HUT", "JPM", "ETSY", "QCOM", "XOM", "PG", "TGT", "ERJ", "MMM", "WFC", "JD", "TEF", "ITUB", "VZ", "GE", "MO", "MCD", "WBA", "HD", "AZN", "ERIC", "COST", "V", "HSY", "CDE", "CVX", "TWLO", "TSM", "SPOT", "NFLX", "SQ", "TRIP", "CAT", "TX", "CRM", "BA", "BIDU", "BG", "DOCU", "PEP", "DESP", "ADBE", "FSLR", "GM", "MU", "AAL", "UNH", "LYG", "BIOX", "MOS", "NUE", "LRCX", "RIO", "IBM", "ABBV", "SBS", "PSX", "NG", "ABEV", "UAL", "F", "UL", "GGB", "FCX", "MSTR", "AIG", "CSCO", "MA", "AMGN", "BP", "XP", "HOG", "CBD", "PHG", "SONY", "DE", "SBUX", "LMT", "UGP", "SHEL", "MRK", "CX", "USB", "SID", "SNOW", "NEM", "TXN", "GSK", "PANW", "BBVA", "ABT", "NOK", "MSI", "TMO", "DOW", "EBAY", "HON", "SPGI", "YY", "RBLX", "INFY", "FDX", "SNAP", "SAN", "ORCL", "LLY", "ADI", "TM", "GS", "DD", "BHP", "EBR", "HL", "NTES", "HAL", "VOD", "NTCO", "PKX", "AEM", "GLW", "UBER", "GPRK", "TTE", "SLB", "AMAT", "UNP", "GILD", "GFI", "WB", "RTX", "BIIB", "ELP", "PBI", "XRX", "ORAN", "HDB", "SYY", "BSBR", "TV", "EA", "HSBC", "AVY", "BK", "CL", "CAH", "AKO/B", "MDT", "E", "KMB", "SCCO", "IBN", "KB", "GRMN", ]
 
-    search_pattern = 'nasdaq_screener'
-    file_path = None
+    json_data = requests.get(
+        'https://api.nasdaq.com/api/screener/stocks?tableonly=true&limit=25&offset=100&download=true',
+        headers=header,
+        timeout=10
+    )
 
-    matching_files = []
+    data = json.loads(json_data.text)
+    rows = data["data"]["rows"]
 
-    for file in os.listdir(download_path):
-        if file.startswith(search_pattern):
-            matching_files.append(file)
-    
-    print(matching_files)
+    df = pd.DataFrame(rows)
 
-    for file in matching_files:
-        file_path = os.path.join(download_path, file)
-        break
+    df["lastsale"] = df["lastsale"].str.replace("$", "").astype(float)
+    df["pctchange"] = df["pctchange"].str.replace("%", "").astype(float)
+    df["name"] = df["name"].str.slice(0, 40)
+    df.rename(columns={"symbol": "especie", "name": "nombre", "lastsale": "ultimo", "pctchange": "var"}, inplace=True)
+    df_filtered = df[df['especie'].isin(usa_list)]
+    df_clean = df_filtered.drop(["netchange", "marketCap", "country", "ipoyear", "volume", "sector", "industry", "url"], axis=1)
+    dt_object = datetime.now()
+    df_clean['hora'] = dt_object
+    df_clean.set_index("especie", inplace=True)
 
-    print(file_path)
-
-    if file_path:
-        csv = file_path
-        df = pd.read_csv(csv)
-        os.remove(file_path)
-        print(f"File '{file_path}' has been deleted.")
-        df = df.rename(columns={"Symbol": "especie", "Name": "nombre", "Last Sale": "ultimo", "% Change": "var"})
-
-        df_filtered = df[df['especie'].isin(usa_list)]
-
-        df_filtered['ultimo'] = df_filtered['ultimo'].str.replace('$', '').astype(float)
-        df_filtered['var'] = df_filtered['var'].str.replace('%', '').astype(float)
-        df_filtered['nombre'] = df_filtered['nombre'].str.slice(0, 40)
-        df_clean = df_filtered.drop(["Net Change", "Market Cap", "Country", "IPO Year", "Volume", "Sector", "Industry"], axis=1)
-        dt_object = datetime.now()
-        df_clean['hora'] = dt_object
-        df_clean.set_index('especie', inplace=True)
-        return df_clean
-    else:
-        print("No file matching the search pattern was found.")
+    return df_clean
