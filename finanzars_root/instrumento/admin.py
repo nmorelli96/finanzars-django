fd1cb001164d5ace7bb3948c82bb6305944e445e
nmorelli96 <morellinicolas96@gmail.com>
2023-09-16 20:26:16 -0300

    Agregar recepcion de nasdaq data

--- finanzars_root/instrumento/admin.py
+++ finanzars_root/instrumento/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tipo, Activo, Especie, Especie_USA
+from .models import Tipo, Activo, Especie, Especie_USA, Nasdaq_Data
 from cartera.models import Operacion
 from cuentas.models import Watchlist, UserProfile
 from dolar.models import Fiat, Banco, Binance, Cryptos, Update
@@ -10,6 +10,7 @@ admin.site.register(Especie)
 admin.site.register(Activo)
 admin.site.register(Operacion)
 admin.site.register(Especie_USA)
+admin.site.register(Nasdaq_Data)
 admin.site.register(Watchlist)
 admin.site.register(Fiat)
 admin.site.register(Banco)
@@ -17,7 +18,6 @@ admin.site.register(Binance)
 admin.site.register(Cryptos)
 admin.site.register(Update)
 admin.site.register(UserProfile)
-
 
 
 
